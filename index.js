6a7a810d717c7495d924895d83e89682d8afe04d
SulavSinghal <sulavsinghal01@gmail.com>
2025-07-22 12:38:59 +0530

    repository setup done

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// CORS Configuration
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001']; // For client and admin-client
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// A simple test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
